eb762d5cdfca136367ab79f3ff756edb55f2d95f
Abl-e <1178972097@qq.com>
2017-09-09 21:51:05 +0800

    修改Message实体类

--- src/main/java/com/tangguoxiang/domain/Message.java
+++ src/main/java/com/tangguoxiang/domain/Message.java
@@ -5,18 +5,30 @@ import java.util.Map;
 
 /**
  * Created by 唐国翔 on 2017/9/7 0007.
+ * 包装返回值数据模型层
  */
 public class Message {
     private Integer code;
     private String msg;
     private Map<String,Object> result = new HashMap<>();
 
+    /**
+     * 返回成功方法
+     * @param message 返回信息
+     * @return Message
+     */
     public static Message success(String message){
         Message msg = new Message();
         msg.setCode(200);
         msg.setMsg(message);
         return msg;
     }
+
+    /**
+     * 返回失败方法
+     * @param message 返回信息
+     * @return Message
+     */
     public static Message fail(String message){
         Message msg = new Message();
         msg.setCode(505);
@@ -24,6 +36,12 @@ public class Message {
         return msg;
     }
 
+    /**
+     * 返回数据
+     * @param key string类型
+     * @param value object类型
+     * @return Message
+     */
     public Message add(String key,Object value){
         this.getResult().put(key,value);
         return this;
